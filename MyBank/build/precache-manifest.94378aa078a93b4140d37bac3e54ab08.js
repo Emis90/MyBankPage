3af73574cdb5afdac509cd0256c0c5c7e954e30d
enida vezelaj <enida@myerrunds.com>
2020-06-05 17:27:51 -0400

    deployed

---
+++ MyBank/build/precache-manifest.94378aa078a93b4140d37bac3e54ab08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb7f663fa207705454c6ba617735270",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee582796814061e60944",
+    "url": "./static/css/main.ca04d04c.chunk.css"
+  },
+  {
+    "revision": "59b2fbbaddd6e0e6d81a",
+    "url": "./static/js/2.4655e718.chunk.js"
+  },
+  {
+    "revision": "eac2959c3141e40bacc1987950abc6c9",
+    "url": "./static/js/2.4655e718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee582796814061e60944",
+    "url": "./static/js/main.42d1f971.chunk.js"
+  },
+  {
+    "revision": "f0eabe31d204b9cf588c",
+    "url": "./static/js/runtime-main.86480c2a.js"
+  }
+]);
